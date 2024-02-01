0f5dc2c758b69db7329af5fec1f982cb7fd2561d
QuixDK <stroganovdanila@outlook.com>
2024-02-01 19:30:25 +0700

    develop add update and get clients api

--- src/main/java/ru/dynamika/library/controller/ClientsController.java
+++ src/main/java/ru/dynamika/library/controller/ClientsController.java
@@ -1,16 +1,33 @@
 package ru.dynamika.library.controller;
 
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import ru.dynamika.library.DTO.ClientDTO;
+import ru.dynamika.library.model.Book;
+import ru.dynamika.library.model.Client;
+import ru.dynamika.library.service.ClientServiceImpl;
 
 @RestController
+@RequiredArgsConstructor
 public class ClientsController {
+
+    @Autowired
+    private final ClientServiceImpl clientService;
 
     @PostMapping("/api/addClient")
     public void addNewClient(@RequestBody ClientDTO clientDTO) {
+        clientService.saveNewClient(clientDTO);
+    }
 
+    @GetMapping("/api/getClients")
+    public String getAllClients() {
+        return clientService.getAllClients();
+    }
+
+    @PutMapping("/api/updateBook")
+    public String updateBook(@RequestBody Client client) {
+        return clientService.updateClient(client);
     }
 }
